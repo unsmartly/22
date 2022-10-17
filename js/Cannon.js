70848c670e81ad885d34250a93501aa23e44d5cb
unsmartly <111931454+unsmartly@users.noreply.github.com>
2022-10-17 16:03:38 -0700

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,21 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonImg=loadImage("assets/canon.png")
+    this.cannonbaseImg=loadImage("assets/cannonBase.png")
+    
+  }
+  display(){
+    push();
+    imageMode(CENTER);
+image(this.cannonImg,this.x,this.y,this.width,this.height)
+    pop();
+image(this.cannonbaseImg,70,20,200,200)
+noFill()
+  }
+}
